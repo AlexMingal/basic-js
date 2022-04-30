e715d7c179bd12edd1021dd6fe5dccf68c52a5f3
AlexMingal <amingalev@gmail.com>
2022-04-30 22:21:33 +0200

    feat: Hanoi towers

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,10 +14,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+
+  let counter = 0,
+      time = 0,
+      resObj = {};
+
+
+      /* recursion wasn't needed :((( 
+  function Solve(n, a,b,c) {
+      if (n > 0) {
+          counter++;
+          Solve(n-1, a, c, b);
+ //       console.log('Перемещаем диск из: ', a, ' на: ', b);
+          Solve(n-1, c, b, a);
+      }
+ //     console.log(counter);
+      return counter;
+    }
+  Solve(disksNumber, '1', '2', '3');
+  
+  END OF RECURSION   */
+
+  counter = (Math.pow(2, disksNumber)) - 1;
+  resObj.turns = counter;
+  resObj.seconds = Math.floor(counter / turnsSpeed * 3600);
+
+ // console.log(resObj);
+  return resObj;
 }
+
+
+// calculateHanoi(9, 4308);
+
 
 module.exports = {
   calculateHanoi
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) { throw new Error(`'arr' parameter must be an instance of the Array!`)};
+
+  let x = '',
+      y = '',
+      result = arr;
+
+  for (i = 0; i <= arr.length; i++) {
+
+    if (arr[i] == '--double-next') {
+      if (i == arr.length-1) {result.splice(i, 1)}
+      else {
+        y = arr.indexOf(arr[i]) + 1;
+        result.splice(i, 1, y);
+      }
+    }  
+
+    if (arr[i] == '--double-prev') {
+      if (i == 0) { result.splice(i, 1)}
+      else {
+      y = arr[i-1];
+      result.splice(i, 1, y);
+      
+      }
+    }  
+
+    if (arr[i] == '--discard-next') {
+      if (i == arr.lentgh-1) { result.splice(i, 1)}
+      else result.splice(i, 2)
+      i--;
+    }  
+
+    if (arr[i] == '--discard-prev') {
+      if (i == 0 || arr[i] == 'undefined') { result.splice(i, 1);  i--;}
+      else result.splice(i-1, 2);
+
+    }  
+
+  }
+  console.log(result.join(','));
+  return result;
 }
+
+transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]);
 
 module.exports = {
   transform
